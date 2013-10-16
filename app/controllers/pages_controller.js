7cd62dced5c36225d746d4cb0ebe3a3236d0ed73
Nam Dao <raikatz@gmail.com>
2013-10-16 13:55:20 +0100

    Initial checkin

---
+++ app/controllers/pages_controller.js
@@ -0,0 +1,16 @@
+var locomotive = require('locomotive')
+  , Controller = locomotive.Controller;
+
+var PagesController = new Controller();
+
+PagesController.main = function() {
+    this.title = 'Locomotive'
+    this.render();
+}
+
+PagesController.home = function() {
+  this.title = 'Locomotive'
+  this.render();
+}
+
+module.exports = PagesController;
