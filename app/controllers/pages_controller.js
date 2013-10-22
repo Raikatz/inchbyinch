1f1e8332df6c15204be71271483f4d9f843cb891
Nam Dao <raikatz@gmail.com>
2013-10-22 11:40:23 +0100

    nodejs module experiment

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -1,11 +1,12 @@
 var locomotive = require('locomotive')
-  , Controller = locomotive.Controller;
+    , Controller = locomotive.Controller
+    , HomeViewModel = require('../viewmodels/HomeViewModel');
 
 var PagesController = new Controller();
 
 
 PagesController.home = function() {
-  this.title = 'Home'
+  this.Model = new HomeViewModel();
   this.render();
 }
 
---
+++ app/viewmodels/HomeViewModel.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Raikatz on 19/10/13.
+ */
+module.exports = function HomeViewModel() {
+    this.Title = 'Home';
+    this.LastDate = new Date().getDate();
+}
